f6a40e0f062525948d871d711f8a10ab3a7d8f47
JPablo1106 <114894139+JPablo1106@users.noreply.github.com>
2024-09-10 13:55:06 -0600

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+// Home.jsx
+import React from 'react';
+import '../css/Home.css'; // Importa el archivo CSS
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Bienvenidos a Librería "Pablín"</h1>
+      <p className="home-description">This is the main content of the Home page.</p>
+    </div>
+  );
+};
+
+export default Home;
